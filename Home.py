45b794533b6c413133e8650d5af754bf420a5210
misagamisaga <79499876+misagamisaga@users.noreply.github.com>
2024-07-30 07:31:43 +0800

    Update Home.py

--- Home.py
+++ Home.py
@@ -101,7 +101,7 @@ def login():
         # st.write(users_info)
         user_name = st.text_input("用户名")
         user_mima = st.text_input("密码", type="password")
-        button_col1, button_col2, button_col3 = st.columns([1,7,1])
+        button_col1, button_col2, button_col3 = st.columns([1,7,1.2])
         with button_col1:
             submit_button1 = st.form_submit_button("登录")
         with button_col2:
